aa1d63025249fee02c7a9960cc4b2a6824d7cf9e
Thom Wright <github@thomwright.co.uk>
2016-05-11 21:13:58 +0100

    Add haddock doc comments

--- src/BristolTransportApi/ImportSources.hs
+++ src/BristolTransportApi/ImportSources.hs
@@ -5,6 +5,7 @@
 module BristolTransportApi.ImportSources
 ( ImportSourcesApi
 , ImportSourcesResponse
+, ImportSource(..)
 ) where
 
 import Data.Aeson
@@ -15,20 +16,34 @@ import Servant.API
 
 import BristolTransportApi.ResponseBody
 
+-- | Retrieve a list of all import sources (i.e. datasets) in the system
+--
+-- This method is used to discover the externally imported data sets that make up the data within the system.
+-- Each data set is identified by an 'ImportSourceID', which can be used to retrieve lists of Transit Agencies, TransitStops and more.
+type ImportSourcesApi =  "importsources" :> Header "X-Api-Key" Text :> Get '[JSON] ImportSourcesResponse
+
+type ImportSourcesResponse = ResponseBody [ImportSource]
+
+-- | Import source data
 data ImportSource = ImportSource
   { name :: Maybe String
+    -- ^ The name of the dataset (where available)
   , importSourceID :: Maybe String
+    -- ^ The unique identifier representing the source of the data. Used as a foreign key in some objects, e.g. TransitAgency
   , attributionLabel :: Maybe String
   , attributionImageURL :: Maybe String
+    -- ^ An attribution graphic for the data
+    --
+    -- Clients MUST display either this label or the string contained at 'attributionLabel' to conform with the Terms and Conditions of using the API.
   , attributionNotes :: Maybe String
+    -- ^ Optional guidance explaining how the AttributionText and AttributionImageURL should be displayed
   , comments :: Maybe String
+    -- ^ Optional comments to clarify the source of the data or explain its origin
   , sourceInfoURL :: Maybe String
+    -- ^ Optional URL to a web page describing the source of the data
   , sourceDataURL :: Maybe String
+    -- ^ Optional URL linking to the external data itself
   } deriving (Generic, Show)
 
 instance ToJSON ImportSource
 instance FromJSON ImportSource
-
-type ImportSourcesResponse = ResponseBody [ImportSource]
-
-type ImportSourcesApi =  "importsources" :> Header "X-Api-Key" Text :> Get '[JSON] ImportSourcesResponse
--- src/BristolTransportApi/ResponseBody.hs
+++ src/BristolTransportApi/ResponseBody.hs
@@ -1,17 +1,24 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module BristolTransportApi.ResponseBody
-( ResponseBody
+( ResponseBody(..)
 ) where
 
 import Data.Aeson
 import Data.Aeson.Types
 import GHC.Generics
 
+-- | Response body shape
 data ResponseBody theRealData = ResponseBody
   { _success :: Maybe Bool
+    -- ^ Indicates either success or a 'soft failure' of the operation
   , _localizedErrorMessage :: Maybe String
+    -- ^ An error message describing why a soft failure occurred
+    --
+    -- Where possible, this will be localized based upon the Accept-Language header passed to the API.
+    -- If Success is TRUE then this field will be omitted from the response.
   , _data :: Maybe theRealData
+    -- ^ Contains the main data of the API response. The Type of the data is endpoint-specific
   } deriving (Generic, Show)
 
 instance ToJSON a => ToJSON (ResponseBody a) where
